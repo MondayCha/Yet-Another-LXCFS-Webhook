44c5814dd1b5d9c9776db4f35fc7416e31f3f674
MondayCha <45418105+MondayCha@users.noreply.github.com>
2025-05-06 18:22:09 +0800

    feat: lxcfs webhook

--- internal/webhook/v1/pod_webhook.go
+++ internal/webhook/v1/pod_webhook.go
@@ -19,6 +19,8 @@ package v1
 import (
 	"context"
 	"fmt"
+	"slices"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -35,28 +37,29 @@ var podlog = logf.Log.WithName("pod-resource")
 // SetupPodWebhookWithManager registers the webhook for Pod in the manager.
 func SetupPodWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).For(&corev1.Pod{}).
-		WithValidator(&PodCustomValidator{}).
-		WithDefaulter(&PodCustomDefaulter{}).
+		WithValidator(&PodLxcfsValidator{}).
+		WithDefaulter(&PodLxcfsDefaulter{}).
 		Complete()
 }
 
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
-// +kubebuilder:webhook:path=/mutate--v1-pod,mutating=true,failurePolicy=fail,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod-v1.kb.io,admissionReviewVersions=v1
+// nolint:lll
+// +kubebuilder:webhook:path=/mutate--v1-pod,mutating=true,failurePolicy=ignore,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=mpod-v1.kb.io,admissionReviewVersions=v1
 
-// PodCustomDefaulter struct is responsible for setting default values on the custom resource of the
+// PodLxcfsDefaulter struct is responsible for setting default values on the custom resource of the
 // Kind Pod when those are created or updated.
 //
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as it is used only for temporary operations and does not need to be deeply copied.
-type PodCustomDefaulter struct {
+type PodLxcfsDefaulter struct {
 	// TODO(user): Add more fields as needed for defaulting
 }
 
-var _ webhook.CustomDefaulter = &PodCustomDefaulter{}
+var _ webhook.CustomDefaulter = &PodLxcfsDefaulter{}
 
 // Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind Pod.
-func (d *PodCustomDefaulter) Default(ctx context.Context, obj runtime.Object) error {
+func (d *PodLxcfsDefaulter) Default(ctx context.Context, obj runtime.Object) error {
 	pod, ok := obj.(*corev1.Pod)
 
 	if !ok {
@@ -64,62 +67,133 @@ func (d *PodCustomDefaulter) Default(ctx context.Context, obj runtime.Object) er
 	}
 	podlog.Info("Defaulting for Pod", "name", pod.GetName())
 
-	// TODO(user): fill in your defaulting logic.
+	// 检查是否需要变异
+	if !mutationRequired(pod) {
+		podlog.Info("Skipping mutation for Pod", "name", pod.GetName(), "namespace", pod.GetNamespace())
+		return nil
+	}
+
+	// 添加变异注解
+	if pod.Annotations == nil {
+		pod.Annotations = make(map[string]string)
+	}
+	pod.Annotations[AdmissionWebhookAnnotationStatusKey] = "mutated"
+
+	// 添加 LXCFS VolumeMounts
+	for i := range pod.Spec.Containers {
+		container := &pod.Spec.Containers[i]
+		if container.VolumeMounts == nil {
+			container.VolumeMounts = make([]corev1.VolumeMount, 0)
+		}
+		container.VolumeMounts = append(container.VolumeMounts, VolumeMountsTemplate...)
+	}
+
+	// 添加 LXCFS Volumes
+	if pod.Spec.Volumes == nil {
+		pod.Spec.Volumes = make([]corev1.Volume, 0)
+	}
+	pod.Spec.Volumes = append(pod.Spec.Volumes, VolumesTemplate...)
 
 	return nil
+}
+
+// mutationRequired 检查是否需要变异
+func mutationRequired(pod *corev1.Pod) bool {
+	// 跳过系统命名空间
+	if slices.Contains(IgnoredNamespaces, pod.Namespace) {
+		podlog.Info("Skip mutation for system namespace", "namespace", pod.Namespace)
+		return false
+	}
+
+	annotations := pod.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+
+	// 检查是否已经变异过
+	if status, ok := annotations[AdmissionWebhookAnnotationStatusKey]; ok && status == "mutated" {
+		return false
+	}
+
+	// 检查是否显式禁用
+	if mutate, ok := annotations[AdmissionWebhookAnnotationMutateKey]; ok {
+		switch strings.ToLower(mutate) {
+		case "false":
+			return false
+		}
+	}
+
+	return true
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
-// +kubebuilder:webhook:path=/validate--v1-pod,mutating=false,failurePolicy=fail,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=vpod-v1.kb.io,admissionReviewVersions=v1
+// nolint:lll
+// +kubebuilder:webhook:path=/validate--v1-pod,mutating=false,failurePolicy=ignore,sideEffects=None,groups="",resources=pods,verbs=create;update,versions=v1,name=vpod-v1.kb.io,admissionReviewVersions=v1
 
-// PodCustomValidator struct is responsible for validating the Pod resource
+// PodLxcfsValidator struct is responsible for validating the Pod resource
 // when it is created, updated, or deleted.
 //
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as this struct is used only for temporary operations and does not need to be deeply copied.
-type PodCustomValidator struct {
+type PodLxcfsValidator struct {
 	// TODO(user): Add more fields as needed for validation
 }
 
-var _ webhook.CustomValidator = &PodCustomValidator{}
+var _ webhook.CustomValidator = &PodLxcfsValidator{}
 
 // ValidateCreate implements webhook.CustomValidator so a webhook will be registered for the type Pod.
-func (v *PodCustomValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+func (v *PodLxcfsValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
 	pod, ok := obj.(*corev1.Pod)
 	if !ok {
 		return nil, fmt.Errorf("expected a Pod object but got %T", obj)
 	}
 	podlog.Info("Validation for Pod upon creation", "name", pod.GetName())
 
-	// TODO(user): fill in your validation logic upon object creation.
-
-	return nil, nil
+	return validate(pod)
 }
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type Pod.
-func (v *PodCustomValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+func (v *PodLxcfsValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (
+	admission.Warnings, error,
+) {
 	pod, ok := newObj.(*corev1.Pod)
 	if !ok {
 		return nil, fmt.Errorf("expected a Pod object for the newObj but got %T", newObj)
 	}
 	podlog.Info("Validation for Pod upon update", "name", pod.GetName())
 
-	// TODO(user): fill in your validation logic upon object update.
-
-	return nil, nil
+	return validate(pod)
 }
 
 // ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type Pod.
-func (v *PodCustomValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+func (v *PodLxcfsValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
 	pod, ok := obj.(*corev1.Pod)
 	if !ok {
 		return nil, fmt.Errorf("expected a Pod object but got %T", obj)
 	}
 	podlog.Info("Validation for Pod upon deletion", "name", pod.GetName())
 
-	// TODO(user): fill in your validation logic upon object deletion.
+	return nil, nil
+}
+
+// Validate, 如果 Pod 存在 AdmissionWebhookAnnotationMutateKey 注解，
+// 只能为 true 或者 false，其他值会被拒绝
+func validate(pod *corev1.Pod) (admission.Warnings, error) {
+	annotations := pod.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+
+	if mutate, ok := annotations[AdmissionWebhookAnnotationMutateKey]; ok {
+		switch strings.ToLower(mutate) {
+		case "true", "false":
+			return nil, nil
+		default:
+			return nil, fmt.Errorf("invalid value for %s annotation: %s", AdmissionWebhookAnnotationMutateKey, mutate)
+		}
+	}
 
 	return nil, nil
 }
